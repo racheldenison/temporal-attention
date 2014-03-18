2b38963e3ce2e1bb26e10adfd80b2f49635754eb
Rachel Denison <r.n.denison@gmail.com>
2014-03-18 15:42:36 -0400

    added comments to drawPlaceholders.m

--- drawPlaceholders.m
+++ drawPlaceholders.m
@@ -2,8 +2,11 @@ function drawPlaceholders(window, color, bgColor, rect, penWidth, on)
 
 % function drawPlaceholders(window, color, bgColor, rect, penWidth, [on=1])
 %
-% Draws 4 corners of a square outline specified by a PTB rect of the form
-% [left top right bottom]
+% Draws 4 corners of a rectangle outline specified by a PTB rect of the form
+% [left top right bottom]. Does this by drawing a rectangle outline and then
+% drawing filled rectangles that are a bit thinner and taller / a bit
+% shorter and wider in the background color, on top of the outline
+% rectangle.
 %
 % Rachel Denison
 % March 2014
