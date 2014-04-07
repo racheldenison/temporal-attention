b0cc9a8b0c53849bacc8bd20142cbc4b83cdb39e
Rachel Denison <r.n.denison@gmail.com>
2014-04-07 12:19:50 -0400

    adjusted params - precue duration, triangle target size

--- temporalAttentionParams.m
+++ temporalAttentionParams.m
@@ -21,7 +21,7 @@ end
 
 p.keyCodes = KbName(p.keyNames);
 p.backgroundColor = 0.5;
-p.nReps = 1;
+p.nReps = 4;
 p.nTrialsPerBlock = 64;
 p.font = 'Verdana';
 p.fontSize = 24;
@@ -34,31 +34,31 @@ p.respInterval = [1 2]; % [1=early 2=late]
 p.cueValidity = [1 -1 0]; % [1=valid -1=invalid 0=neutral]
 % p.propValid = 0.67;
 % p.cueValidityFactor = generatePropFactor(p.propValid);
-p.cueValidityFactor = [1 1 1 2 3]; % eg. [1 1 2 3] is 50% valid, 25% invalid, 25% neutral
+p.cueValidityFactor = [3]; % eg. [1 1 2 3] is 50% valid, 25% invalid, 25% neutral
 % p.cueValidityFactor = 3;
 p.propValid = nnz(p.cueValidityFactor==1)./nnz(p.cueValidityFactor<3);
 p.propNeutral = nnz(p.cueValidityFactor==3)./numel(p.cueValidityFactor);
 
 % Timing
 p.soas = [1000 1250]/1000; % [short long]
-p.preCueDur = 0.75; % time between fixation onset and cue
+p.preCueDur = 1.1; % time between fixation onset and cue
 p.cueDur = 0.2;
 p.targetDur = 3/100; % 30 ms / 33 ms
 p.maskSOA = 5/100; % 4/60 time between target onset and mask onset
 p.maskDur = 1/100; % 1/60, 3/60
 p.respCueSOA = p.soas(2) + 0.5;
-p.iti = 0.5; % inter-trial interval
+p.iti = 0.5; % inter-trial interval (also, the duration of the feedback symbol)
 
 % Images
 p.imPos = [4 4];
 p.imSize = [4 4]; % this is the size of the image container that holds the stim
 p.targetSize = 0.5; % 0.5 sigma of gaussian / 1.5 side length of T/L / 1.5 width of triangle
 p.spatialFrequency = 4; % 4
-p.targetOrientation = [-2 2]; % eg. [-10 10]
+p.targetOrientation = [-5 5]; % eg. [-10 10]
 p.TL = [0 0.5]; % [offset-for-T(=0) offset-for-L]
 p.TLLineWidth = 5; % (pixels)
 p.triangleBlurProp = 0.3;
-p.triHWRatio = 1.5; % should be > 1 so that targetSize corresponds to triangle width
+p.triHWRatio = 1; % should be >= 1 so that targetSize corresponds to triangle width
 p.extraOblTilt = [-5 5];
 
 % Task
@@ -71,7 +71,7 @@ end
 % target rotation
 switch p.task
     case 'targetOrientation'
-        p.rotateTarget = 'cardobl'; % 'none','rotT2'= rotate T2 90 deg, 'rotT1' = rotate T1 90 deg, 'cb'= counterbalanced vert/horiz, 'cardobl'= cardinal + oblique orientations
+        p.rotateTarget = 'card4'; % 'none','rotT2'= rotate T2 90 deg, 'rotT1' = rotate T1 90 deg, 'cb'= counterbalanced vert/horiz, 'cardobl'= cardinal + oblique orientations, 'card4'= 4 cardinal directions (triangle)
     case 'spatialFrequency'
         p.rotateTarget = 'random'; % random rotations
     case 'TL'
@@ -81,7 +81,7 @@ end
 %%% NB. currently, rotated targets can obscure placeholders
 if strcmp(p.rotateTarget, 'card4')
     p.aperture = 'triangle'; % 'gaussian','triangle'
-    p.targetSize = 1.5;
+    p.targetSize = 2;
 else
     p.aperture = 'gaussian';
 end
