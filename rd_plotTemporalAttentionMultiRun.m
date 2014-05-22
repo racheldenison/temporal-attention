81837b2992408efd0b1136c6b63620fc4f4d4d3d
Rachel Denison <r.n.denison@gmail.com>
2014-05-22 13:46:14 -0400

    added rd_plotTemporalAttentionMultiRun, to plot data from individual runs in one condition from one subject

--- rd_plotTemporalAttentionMultiRun.m
+++ rd_plotTemporalAttentionMultiRun.m
@@ -1,49 +1,43 @@
-% rd_plotTemporalAttentionMultiSOA.m
+% rd_plotTemporalAttentionMultiRun.m
 
-subjectInit = 'id';
+subjectInit = 'rd';
 exptName = 'cb';
-tilt = '1pt5';
-contrast = 16;
+tilt = '2';
+contrast = '64';
+contrastIdx = 1; % only plot one contrast at a time
+soa1 = 1000;
+soa2 = 1250;
 
-soa1 = [1000 1000 1000];
-soa2 = [1100 1250 2500];
-t1t2soa = soa2 - soa1;
-run = 9;
+runs = 1:3;
+
+saveFigs = 1;
 
 dataDir = pathToExpt('data');
+% dataDir = [pathToExpt('data') '/pilot/rd'];
 
-subjectID = sprintf('%s_%s_tilt%s_tc%d', ...
-    subjectInit, exptName, tilt, contrast);
-
+subjectID = sprintf('%s_%s_tilt%s_tc%s_soa%d-%d', ...
+    subjectInit, exptName, tilt, contrast, soa1, soa2);
 
 %% Get data
-for iSOA = 1:numel(soa1)
-    subject = sprintf('%s_soa%d-%d', ...
-        subjectID, soa1(iSOA), soa2(iSOA));
+for iRun = 1:numel(runs)
+    run = runs(iRun);
     
     % load data from a given soa
     dataFile = dir(sprintf('%s/%s_run%02d*', ...
-        dataDir, subject, run));
+        dataDir, subjectID, run));
     if numel(dataFile)~=1
+        sprintf('%s/%s_run%02d*', dataDir, subjectID, run)
         error('more or fewer than one matching data file')
     else
         load(sprintf('%s/%s', dataDir, dataFile.name))
     end
     
-    % read out the target timings
-    soas(iSOA,:) = expt.p.soas;
-    
-    % check soas
-    if soas(iSOA,1)~=soa1(iSOA)/1000 || soas(iSOA,2)~=soa2(iSOA)/1000
-        error('requested SOAs do not match SOAs in data file')
-    end
-    
     % read out the accuracy and rt
     for iEL = 1:2 % early/late
-        accMean{iEL}(:,iSOA) = results.accMean{iEL};
-        accSte{iEL}(:,iSOA) = results.accSte{iEL};
-        rtMean{iEL}(:,iSOA) = results.rtMean{iEL};
-        rtSte{iEL}(:,iSOA) = results.rtSte{iEL};
+        accMean{iEL}(:,iRun) = results.accMean{iEL}(:,contrastIdx);
+        accSte{iEL}(:,iRun) = results.accSte{iEL}(:,contrastIdx);
+        rtMean{iEL}(:,iRun) = results.rtMean{iEL}(:,contrastIdx);
+        rtSte{iEL}(:,iRun) = results.rtSte{iEL}(:,contrastIdx);
     end
 end
 
@@ -52,8 +46,8 @@ p = expt.p;
 %% Plot figs
 intervalNames = {'early','late'};
 accLims = [0.2 1];
-rtLims = [0.3 1.6];
-soaLims = [t1t2soa(1)-100 t1t2soa(end)+100];
+rtLims = [0.3 1.6]; % [0.3 1.6]
+xlims = [runs(1)-1 runs(end)+1];
 colors = get(0,'DefaultAxesColorOrder');
 axTitle = '';
 
@@ -61,16 +55,17 @@ fig(1) = figure;
 for iRI = 1:numel(p.respInterval)
     subplot(1,numel(p.respInterval),iRI)
     hold on
-    plot(soaLims, [0.5 0.5], '--k');
+    plot(xlims, [0.5 0.5], '--k');
     
-    p1 = errorbar(repmat(t1t2soa',1,numel(p.cueValidity)),...
+    p1 = errorbar(repmat(runs',1,numel(p.cueValidity)),...
         accMean{iRI}', accSte{iRI}', '.-', 'MarkerSize', 20);
 
-    xlabel('soa')
+    set(gca,'XTick',runs)
+    xlabel('run')
     ylabel('acc')
     legend(p1, num2str(p.cueValidity'),'location','best')
     title(intervalNames{iRI})
-    xlim(soaLims)
+    xlim(xlims)
     ylim(accLims)
     rd_supertitle(subjectID);
     rd_raiseAxis(gca);
@@ -81,17 +76,23 @@ fig(2) = figure;
 for iRI = 1:numel(p.respInterval)
     subplot(1,numel(p.respInterval),iRI)
     
-    p1 = errorbar(repmat(t1t2soa',1,numel(p.cueValidity)),...
+    p1 = errorbar(repmat(runs',1,numel(p.cueValidity)),...
         rtMean{iRI}', rtSte{iRI}', '.-', 'MarkerSize', 20);
     
-    xlabel('soa')
+    set(gca,'XTick',runs)
+    xlabel('run')
     ylabel('rt')
     legend(num2str(p.cueValidity'),'location','best')
     title(intervalNames{iRI})
-    xlim(soaLims)
+    xlim(xlims)
     ylim(rtLims)
     box off
     rd_supertitle(subjectID);
     rd_raiseAxis(gca);
     rd_supertitle(axTitle);
+end
+
+if saveFigs
+    figNames = {'acc','rt'};
+    rd_saveAllFigs(fig, figNames, sprintf('%s_byRun_TemporalAttention_T1T2all', subjectID))
 end
