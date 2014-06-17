b7ae6e7e7170355d2eb618ff6d2cec7663ef8c5c
Rachel Denison <r.n.denison@gmail.com>
2014-06-17 12:23:14 -0400

    running expt

--- temporalAttentionParams.m
+++ temporalAttentionParams.m
@@ -51,7 +51,7 @@ p.targetDur = 3/100; % 30 ms / 33 ms
 p.maskSOA = 5/100; % 4/60 time between target onset and mask onset
 p.maskDur = 1/100; % 1/60, 3/60
 p.respCueSOA = p.soas(2) + 0.5;
-p.respGoSOA = 0.5; % time between resp cue onset and go onset. set to zero for no go cue.
+p.respGoSOA = 0.8; % time between resp cue onset and go onset. set to zero for no go cue.
 p.iti = 0.5; % inter-trial interval (also, the duration of the feedback symbol)
 p.eyeSlack = 0.1; % cushion between last fixation check and next stimulus presentation
 
