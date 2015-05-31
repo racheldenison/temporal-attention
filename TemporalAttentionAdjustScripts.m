7158c4752bea7b8a17ee5d5e585e9367cdf24f1c
Rachel Denison <r.n.denison@gmail.com>
2015-05-31 19:33:28 -0400

    added TemporalAttentionAdjustScripts.m and TemporalAttentionScripts.m to keep track of the script sequence for analyzing data in these experiments

---
+++ TemporalAttentionAdjustScripts.m
@@ -0,0 +1,15 @@
+% TemporalAttentionAdjustScripts
+
+% combine runs
+rd_combineRunsTemporalAttentionAdjust
+
+% errors sequence (does not depend on fit)
+rd_plotTemporalAttentionAdjustErrorsGroup
+rd_plotTemporalAttentionAdjustBigErrors
+rd_fitDOG
+
+% mixture model fit sequence
+rd_fitTemporalAttentionAdjustGroup
+rd_plotTemporalAttentionAdjustFit 
+rd_organizeAdjustFitGroupStats
+
